a347798ca1d3683cfdc30c6bd937884745660295
Ahhyeon Song <thddkgus36@gmail.com>
2016-09-27 20:37:19 +0900

    #4. add Button and Event

--- app/src/main/java/kr/hs/emirim/thddkgus96/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/thddkgus96/imagetest/MyImage.java
@@ -10,32 +10,58 @@ import android.view.View;
  * Created by appcreator27 on 2016. 9. 26..
  */
 public class MyImage extends View{
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
     MyImage(Context context){
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(), R.drawable.ukwon);
+    }
+
+    public void setChoose(int choose) {
+        this.choose = choose;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
-        Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.ukwon);
+
         float cx=getWidth()/2.0f;
         float cy=getHeight()/2.0f;
 
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
-        //회전
-        //canvas.rotate(45, cx, cy);
 
+        switch(choose){
+
+            case MainActivity.ROTATE:
+                canvas.rotate(45, cx, cy);
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(2,2,cx, cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f, 0.4f);
+                break;
+
+
+        }
+        //회전
+       //
         //이동
-        // canvas.translate(-150,200);
+        //
         //크기
-        // canvas.scale(2,2,cx, cy);
+        //
         //비틀기
-        canvas.skew(0.4f, 0.4f);
+        //
 
 
         canvas.drawBitmap(picture, x, y, null);
+       // picture.recycle();
     }
 
 }
